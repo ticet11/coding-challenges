b1cffa6656c8f1c9a3c36061e580ebc45de3ed06
ticet11 <BriKozub@gmail.com>
2020-04-20 13:32:38 -0600

    Build an HTML Heading Generator Function in Python

--- python/main.py
+++ python/main.py
@@ -1,12 +1,20 @@
-def guessing_game():
-  while True:
-    print('What is your guess?')
-    guess = input()
+"""
+heading_generator(title, heading_type)
 
-    if guess == '42':
-      print('Correct!')
-      return False
-    else:
-      print(f'No go, buddy. {guess} is wrong. Try again.')
+heading_generator('Greeting', '1')
+<h1>Greeting</h1>
 
-guessing_game()
+heading_generator('Howdy', '3')
+<h3>Howdy<h3>
+"""
+content_example = 'Hello'
+tag_example = 'h'
+
+def html_content_generator(content, tag):
+  if tag.lower() == 'h':
+    heading_number = input('Which heading number?')
+    print(f'<{tag}{heading_number}>{content}<{tag}{heading_number}>')
+  else:
+    print(f'<{tag}>{content}<{tag}>')
+
+html_content_generator(content_example, tag_example)
--- python/weighted_lottery_function.py
+++ python/weighted_lottery_function.py
@@ -1,36 +1,27 @@
 import random
 
-weights = {
-    'win' : 1,
-    'break even' : 5,
-    'lose' : 6
+weights_1 = {
+    'win': 1,
+    'break even': 5,
+    'lose': 6
 }
+
 
 def weighted_lottery(weights):
     hat_container = []
-    key_container = list(weights)
-    win_count = weights['win']
-    draw_count = weights['break even']
-    lose_count = weights['lose']
 
-    for win in range(win_count):
-        hat_container.append(key_container[0])
+    for outcome, odds in weights.items():
+        for _ in range(odds):
+            hat_container.append(outcome)
+    random_outcome = random.choice(hat_container)
+    print(f'You {random_outcome}!')
 
-    for draw in range(draw_count):
-        hat_container.append(key_container[1])
-
-    for lose in range(lose_count):
-        hat_container.append(key_container[2])
-
-    outcome = random.choice(hat_container)
-    print(f'You {outcome}')
-
-    play_again = (input('Play again? Yes/No:'))
+    play_again = (input('Play again? Yes/No:\n'))
 
     if play_again.lower().strip() == 'yes':
         weighted_lottery(weights)
-    else: 
+    else:
         print('K, thx, bye.')
 
 
-weighted_lottery(weights)
+weighted_lottery(weights_1)
