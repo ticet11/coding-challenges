13cf2b51585bf5cdc022bf3262418ebc35a25eb1
ticet11 <BriKozub@gmail.com>
2020-04-23 15:33:35 -0600

    Built working cents changer

---
+++ price_changer.py
@@ -0,0 +1,16 @@
+class Item:
+    """An item we sell at our store."""
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+def cents_changer(Item, new_cents):
+    old_price = Item.price
+    Item.price = int(Item.price) + new_cents
+    return f'You have changed the price of {Item.name}s from ${old_price} to ${Item.price}!'
+
+item1 = Item('book', 5.50)
+item2 = Item('pencil', 1.22)
+
+cents_changer(item2, 0.99)
