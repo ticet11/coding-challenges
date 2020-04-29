e5f0f1a5f3196e0782e34fd38936223b616ac21d
ticet11 <BriKozub@gmail.com>
2020-04-28 20:14:29 -0600

    returns true or false if an input is string or not

---
+++ JavaScript/stringChecker.js
@@ -0,0 +1,8 @@
+/*  Write a JavaScript function to check whether an ‘input’ or argument passed into your function is a string or not
+ex: stringChecker(“i am a string”) //returns True
+    stringChecker(100) //returns false */
+
+    function stringChecker(test_subject) {
+        console.log(typeof test_subject === 'string')
+    }
+    
