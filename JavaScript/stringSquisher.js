c2c43ad978ad9942dedf6178a690c819897710fb
ticet11 <BriKozub@gmail.com>
2020-04-28 20:15:14 -0600

    Concatinates 2 strings together.

---
+++ JavaScript/stringSquisher.js
@@ -0,0 +1,6 @@
+/* Write a javascript function that adds two strings together
+ex: stringAdder(“hi”, “there”) // returns “hi there” */
+
+function stringSquisher(stringOne, stringTwo) {
+    console.log(stringOne + ' ' + stringTwo)
+  }
