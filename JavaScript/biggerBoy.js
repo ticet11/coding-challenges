1e17d10ebd2b5e5073d95aa9fd2fe1e48767c65b
ticet11 <BriKozub@gmail.com>
2020-04-28 20:13:15 -0600

    function returns larger of 2 numbers

---
+++ JavaScript/biggerBoy.js
@@ -0,0 +1,6 @@
+/* Write a JavaScript function that accept two integers as arguments and returns the larger number.
+ex: largerNumber(12, 9) // returns 12 */
+
+function biggerBoy(numOne, numTwo) {
+    numOne > numTwo ? console.log(numOne) : console.log(numTwo);
+  }
