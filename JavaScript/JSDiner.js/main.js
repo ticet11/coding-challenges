d4b29344c2b5c95353acc923a4aceda35a3072f6
ticet11 <BriKozub@gmail.com>
2020-05-01 10:46:47 -0600

    Added side interaction. Fixed menu issue where menu was always breakfast.

--- JavaScript/JSDiner.js/main.js
+++ JavaScript/JSDiner.js/main.js
@@ -1,79 +1,101 @@
 const menus = {
-    omletteMenu: {
-      "ham and cheese" : 3,
-      "diablo"         : 4,
-      "Denver"         : 3,
-      "JS special"     : 5
-    },
-  
-    lunchMenu    : {
-      "grilled cheese" : 1,
-      "big burger"     : 5,
-      "little burger"  : 3,
-      "pulled porkwich": 5
-    },
-  
-    dinnerMenu   : {
-      "grilled cheese" : 2,
-      "big burger"     : 6,
-      "little burger"  : 4,
-      "pulled porkwich": 6
-    },
-  
-    sideMenu     : { 
-      "fries"          : 1, 
-      "salad"          : 1, 
-      "bread"          : 1, 
-      "mashed potatoes": 1
-    }
+  omletteMenu: {
+    "ham and cheese" : 3,
+    "diablo"         : 4,
+    "Denver"         : 3,
+    "JS special"     : 5
+  },
+
+  lunchMenu    : {
+    "grilled cheese" : 1,
+    "big burger"     : 5,
+    "little burger"  : 3,
+    "pulled porkwich": 5
+  },
+
+  dinnerMenu   : {
+    "grilled cheese" : 2,
+    "big burger"     : 6,
+    "little burger"  : 4,
+    "pulled porkwich": 6
+  },
+
+  sideMenu     : { 
+    "french fries"   : 1, 
+    "salad"          : 1, 
+    "bread"          : 1, 
+    "mashed potatoes": 1
   }
-  
-  const waiterBotSpeech = {
-    "ham and cheese"    : 'very classic, very cool.',
-    "diablo"            : 'when they say spicy, they mean spicy.',
-    "Denver"            : 'they say that is the place to get your drugs.',
-    'JS Special'        : 'it is a program with no real food.',
-    'grilled cheese'    : 'very classic, very cool.',
-    'big burger'        : 'this burger will make you full.',
-    'little burger'     : 'this burger will make a baby full.',
-    'pulled porkwich'   : 'we will pull the pork just for you.',
-    'fries'             : 'oui.',
-    'salad'             : 'you think you are better than me?',
-    'bread'             : 'we only have pumpernikle and you are okay with that.',
-    'mashed potatoes'   : 'oh, actually, we can not mash those, it will be whole.'
+}
+
+const waiterBotSpeech = {
+  "ham and cheese"    : 'Very classic, very cool.',
+  "diablo"            : 'When they say spicy, they mean spicy.',
+  "denver"            : 'They say that is the place to get your drugs.',
+  'js special'        : 'It is a program with no real food.',
+  'grilled cheese'    : 'Very classic, very cool.',
+  'big burger'        : 'This burger will make you full.',
+  'little burger'     : 'This burger will make a baby full.',
+  'pulled porkwich'   : 'We will pull the pork just for you.',
+  'french fries'      : 'Oui.',
+  'salad'             : 'Some rabbit food for you',
+  'bread'             : 'We only have pumpernikle and you are okay with that.',
+  'mashed potatoes'   : 'Oh, actually, we can not mash those, it will be whole.'
+}
+
+const whichMenu = () => {
+  let menuChoice = prompt('Which menu are we looking at?\nbreakfast,\nlunch,\nor dinner?', 'breakfast');
+  if (menuChoice === 'breakfast') {
+    getMenuItems(menus.omletteMenu);
+  } else if (menuChoice === 'lunch') {
+    getMenuItems (menus.lunchMenu);
+  } else if (menuChoice === 'dinner') {
+    getMenuItems (menus.dinnerMenu);
+  } else {
+    alert("That's not a choice, try again.");
+    whichMenu();
   }
-  
-  const whichMenu = () => {
-    let menuChoice = prompt('Which menu are we looking at?\nbreakfast,\nlunch,\ndinner', 'breakfast');
-    if (menuChoice = 'breakfast') {
-      getMenuItems(menus.omletteMenu);
-    } else if (menuChoice = 'lunch') {
-      getMenuItems (menus.lunchMenu);
-    } else if (menuChoice = 'dinner') {
-      getMenuItems (menus.dinnerMenu);
-    } else {
-      alert("That's not a choice, try again.");
-      whichMenu();
-    }
+}
+
+const getMenuItems = (menu) => {
+  let menuItems = Object.keys(menu);
+  if (menu === menus.sideMenu) {
+    getSides(menuItems);
+  } else {
+    getEntree(menuItems);
   }
-  const getMenuItems = (menu) => (menuItems = Object.keys(menu));
-  const getEntree = (menuItems) => {
-    menuItemsString = "";
-    for (let i of menuItems) {
-      menuItemsString += i + "\n";
+};
+
+const getEntree = (menuItems) => {
+  menuItemsString = "";
+  for (let i of menuItems) {
+    menuItemsString += i + "\n";
     }
-    const entree = prompt(
-      `These are our specials today:\n\n${menuItemsString}\nWhat can I get for you?`,
-      "grilled cheese"
-    );
-    entreeResponse(entree);
-  };
-  
-  const entreeResponse = (customerSpeech) => {
-    alert(`${customerSpeech}? ${waiterBotSpeech[customerSpeech]}`);
-  };
-  
-  whichMenu();
-  getEntree(menuItems);
-  
-  
+  const entree = prompt(
+    `These are our specials, today:\n\n${menuItemsString}\nWhat can I get for you?`,
+    "grilled cheese"
+  );
+  entreeResponse(entree.toLowerCase());
+};
+
+const entreeResponse = (customerSpeech) => {
+  alert(`Oh, ${customerSpeech}? ${waiterBotSpeech[customerSpeech]}`);
+  getMenuItems (menus.sideMenu)
+};
+
+const getSides = (menuItems) => {
+menuItemsString = "";
+for (let i of menuItems) {
+  menuItemsString += i + "\n";
+}
+const side1 = prompt(`And your two sides? I do recommend the french fries.\n\n${menuItemsString}\nFirst Side:\n`, 'french fries');
+sideResponse(side1.toLowerCase());
+const side2 = prompt(`And the other?\n\n${menuItemsString}\nSecond Side:\n`, 'mashed potatoes')
+sideResponse(side2.toLowerCase());
+}
+
+const sideResponse = (customerSpeech) => {
+alert(`Okay, ${customerSpeech}. ${waiterBotSpeech[customerSpeech]}`);
+};
+
+whichMenu();
