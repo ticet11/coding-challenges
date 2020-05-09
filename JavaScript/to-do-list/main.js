d226643596b851a432fa47a9be4118114f328c39
Brian Kozub <ticet11@gmail.com>
2020-05-08 18:04:58 -0600

    Function will strike through completed list items

--- JavaScript/to-do-list/main.js
+++ JavaScript/to-do-list/main.js
@@ -1,11 +1,18 @@
 const items = document.querySelectorAll('.todoItem');
+const incompletedItems = document.getElementById('incompleteTodoWrapper');
 const completedItemsList = document.querySelector('.completeTodoWrapper');
+const completedItems = completedItemsList.querySelectorAll('li').getE;
+const pendingH2 = document.getElementById('pendingTodos');
+console.log(pendingH2);
 
 items.forEach(item => {
     item.addEventListener('click', function completedItem(e) {
         const target = e.target;
         completedItemsList.appendChild(target);
-        
-
+        item.classList.add('completedTodoItem');
+        console.log(incompletedItems.hasChildNodes);
+        if (incompletedItems.hasChildNodes === false) {
+            pendingH2.classList.add('empty');
+        }
     });
 });
