b4d365ea9e56c2f76248055a4cb4e10183863d9e
Brian Kozub <ticet11@gmail.com>
2020-05-09 14:20:54 -0600

    When all items are completed, additional styling is added to pending heading.

--- JavaScript/to-do-list/main.js
+++ JavaScript/to-do-list/main.js
@@ -1,17 +1,15 @@
-const items = document.querySelectorAll('.todoItem');
-const incompletedItems = document.getElementById('incompleteTodoWrapper');
-const completedItemsList = document.querySelector('.completeTodoWrapper');
-const completedItems = completedItemsList.querySelectorAll('li').getE;
-const pendingH2 = document.getElementById('pendingTodos');
-console.log(pendingH2);
+const items = document.querySelectorAll(".todoItem");
+const incompletedItems = document.getElementById("incompleteTodoWrapper");
+const completedItemsList = document.querySelector(".completeTodoWrapper");
+const completedItems = completedItemsList.querySelectorAll("li").getE;
+const pendingH2 = document.getElementById("pendingTodos");
 
-items.forEach(item => {
-    item.addEventListener('click', function completedItem(e) {
+items.forEach((item) => {
+    item.addEventListener("click", function completedItem(e) {
         const target = e.target;
         completedItemsList.appendChild(target);
-        item.classList.add('completedTodoItem');
-        console.log(incompletedItems.hasChildNodes);
-        if (incompletedItems.hasChildNodes === false) {
+        item.classList.add("completedTodoItem");
+        if (incompletedItems.querySelectorAll("li").length === 0) {
             pendingH2.classList.add('empty');
         }
     });
