ed4fd8f03e94b9d8e0d39d8f582cbd6fa34d21cc
Brian Kozub <ticet11@gmail.com>
2020-05-11 20:06:38 -0600

    Updated to check for incorrect inputs.

--- JavaScript/arrayTrimmer.js
+++ JavaScript/arrayTrimmer.js
@@ -1,14 +1,22 @@
 const numArray = [1, 2, 3, 4, 5];
-
+// // Basic functioning trimmer.
 // const arrayTrimmer = function (someArray) {
 //     someArray.pop();
 //     someArray.shift();
 //     return someArray;
 // }
 
-// Cleaner version
+// // Cleaner version
+// const arrayTrimmer = function (someArray) {
+//     return someArray.slice(1, -1);
+// }
+
+// Checks for incorrect inputs.
 const arrayTrimmer = function (someArray) {
+    if (Array.isArray(someArray) === false) return "Please enter an array.";
+    if (someArray.length < 3)
+      return "Please enter an array with at least 3 items.";
     return someArray.slice(1, -1);
-}
+  };
 
 console.log(arrayTrimmer(numArray));
