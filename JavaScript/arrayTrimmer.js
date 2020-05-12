ffa8316efd27b5c44d38a617d5c113a669552860
Brian Kozub <ticet11@gmail.com>
2020-05-11 19:41:44 -0600

    Updated to use slice instead of pop and shift.

--- JavaScript/arrayTrimmer.js
+++ JavaScript/arrayTrimmer.js
@@ -1,9 +1,14 @@
 const numArray = [1, 2, 3, 4, 5];
 
+// const arrayTrimmer = function (someArray) {
+//     someArray.pop();
+//     someArray.shift();
+//     return someArray;
+// }
+
+// Cleaner version
 const arrayTrimmer = function (someArray) {
-    someArray.pop();
-    someArray.shift();
-    return someArray;
+    return someArray.slice(1, -1);
 }
 
 console.log(arrayTrimmer(numArray));
