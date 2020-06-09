cc89fe0b86e0ec8ad17a6a607f8bb807e2c3843f
Brian Kozub <ticet11@gmail.com>
2020-06-08 20:33:09 -0600

    Functional terminal interface, everything seems to be in working order.

--- python/BKCU/main.py
+++ python/BKCU/main.py
@@ -1,8 +1,7 @@
 # Python Bank
-# TODO: 
+# TODO:
+# The program needs to keep running until the user decides to quit the program.
 # user interaction
-#  - atm interface
-#   - greeting
 
 
 class User:
@@ -29,25 +28,25 @@ class Bank:
         self.bankFunds = 0
 
     def retrieve_balance(self, account_number):
-            return account_number.provide_balance()
+        return account_number.provide_balance()
 
     def attempt_withdraw(self, account_number, amount):
-            if account_number.handle_withdraw(amount):
-                self.bankFunds -= amount
-                return f"Here's your ${amount}."
-            else:
-                return 'Insufficient funds.'
+        if account_number.handle_withdraw(amount):
+            self.bankFunds -= amount
+            return f"\nHere's your ${amount}."
+        else:
+            return '\nInsufficient funds.'
 
     def attempt_deposit(self, account_number, amount):
         self.bankFunds += amount
-        return f'Thank you. Your new balance is ${account_number.handle_deposit(amount)}.'
+        return f'\nThank you. Your new balance is ${account_number.handle_deposit(amount)}.'
 
     def attempt_bill_pay(self, account_number, amount, institution):
         if account_number.handle_withdraw(amount):
             self.bankFunds -= amount
-            return f"We've forwarded ${amount} to {institution} for you."
+            return f"\nWe've forwarded ${amount} to {institution} for you."
         else:
-            return 'Insufficient funds.'
+            return '\nInsufficient funds.'
 
 
 class Account():
@@ -78,11 +77,36 @@ class Account():
             return True
 
 
+def atmGreeting():
+
+    action = input(
+        f'\nWelcome to BKCU, {user_test.name}. How can we serve you today?\n"Check Balance"\n"Withdraw"\n"Deposit"\n"Bill Pay"\n"Exit"\n\n').lower()
+    while True:
+        if action == 'check balance':
+            print(f'\nYour balance is ${user_test.request_balance()}.')
+        elif action == 'withdraw':
+            amount = input('\nSure, how much would you like to withdraw?\n$')
+            print(user_test.request_withdraw(int(amount)))
+        elif action == 'deposit':
+            amount = input('\nWonderful, how much will you deposit?\n$')
+            print(user_test.request_deposit(int(amount)))
+        elif action == 'bill pay':
+            institution = input(
+                '\nSure, who are we sending the money to?').lower()
+            amount = input('\nAnd how much would you like to send?\n$')
+            print(user_test.request_bill_pay(int(amount), institution))
+        elif action == 'exit':
+            break
+        else:
+            print('Try again, please.\n')
+        if action != 'exit':
+            action = input(
+                f'\nWhat else can I do for you, {user_test.name}?\n"Check Balance"\n"Withdraw"\n"Deposit"\n"Bill Pay"\n"Exit"\n\n').lower()
+
+    print('\nHave a nice day. Thank you for banking with BKCU.')
+
+
 bank_test = Bank()
 account_test = Account('1')
 user_test = User('Brian', account_test, bank_test)
-user_test.request_deposit(40)
-print(user_test.request_bill_pay(20, 'test company'))
-
-# The user needs to be able to look up their balance
-# The program needs to keep running until the user decides to quit the program.
+atmGreeting()
