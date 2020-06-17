ca80a11363b4ee957fc76c623489519ab35aa7e2
Brian Kozub <ticet11@gmail.com>
2020-06-17 16:49:57 -0600

    refactored letter_search to check for upper and lowercase.

--- python/letter_search.py
+++ python/letter_search.py
@@ -2,12 +2,12 @@ def letter_search(some_string):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     binary_output = ''
     for char in alphabet:
-        if char in some_string:
+        if char in some_string.lower():
             binary_output += '1'
         else:
             binary_output += '0'
     return binary_output
 
-print(letter_search('abc'))
+print(letter_search('Abc'))
             
 
