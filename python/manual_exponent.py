9d4b45325413fe27fa1ddc8e01ab50d95392a077
Brian Kozub <ticet11@gmail.com>
2020-06-16 22:26:09 -0600

    functional manual_exponent function.

---
+++ python/manual_exponent.py
@@ -0,0 +1,10 @@
+import functools
+
+def exponenter(base, exponent):
+    base_array = []
+    rangeNum = range(exponent)
+    for num in rangeNum:
+        base_array.append(base)
+    return functools.reduce(lambda a, b : a * b, base_array)
+
+print(exponenter(2, 2))
