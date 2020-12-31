d9cd42e2c9760b03f6d153f926edeb77633aff4d
Brian Kozub <ticet11@gmail.com>
2020-12-31 13:46:07 -0700

    Beginning spam_filter program

--- python/waiting_game/main.py
+++ python/waiting_game/main.py
@@ -1,4 +1,5 @@
 import time
+import keyboard
 
 """
 * Pick a number that the user will try to stop at.
@@ -10,52 +11,53 @@ the timer will start and the user will have to "guess" when that amount of time 
 * Print out the elapsed time and tell the user if they won or by how much they were over or under
 """
 
-def waiting_game(target_time):
-    input('Press "Enter" to start.')
+start_has_run = False
+end_has_run = False
+
+
+def waiting_game():
+    print('Press "s" to begin the timer.')
+
+    while start_has_run == False:
+        keyboard.add_hotkey('s', lambda: print(start_timer()), )
+
+    print('press "s" to end')
+    while end_has_run == False:
+        keyboard.add_hotkey('e', lambda: print(end_timer()))
+
+
+def start_timer():
     start_time = time.time()
-
-    input('Press "Enter" to stop.')
-    end_time = time.time()
-
-    time_lapsed = end_time - start_time
-    difference = target_time - time_lapsed
-
-    print(f'{round(time_lapsed, 3)} seconds have elapsed.')
-    print(f'You were {round(difference, 3)} seconds off.')
-    if difference < 1:
-        print('You win!')
-    else: 
-        print('You lose!')
+    start_has_run = True
+    return start_time, start_has_run
 
 
-waiting_game(3)
+def end_timer():
+    stop_time = time.time()
+    end_has_run = True
+    return stop_time
 
 
-# import keyboard
-# start_has_run = False
-# end_has_run = False
+print(waiting_game())
+
+# def waiting_game(target_time):
+#     input('Press "Enter" to start.')
+#     start_time = time.time()s
+
+#     input('Press "Enter" to stop.')
+#     end_time = time.time()
+
+#     time_lapsed = end_time - start_time
+#     difference = target_time - time_lapsed
+
+#     print(f'{round(time_lapsed, 3)} seconds have elapsed.')
+#     print(f'You were {round(difference, 3)} seconds off.')
+#     if difference < 1:
+#         print('You win!')
+#     else: 
+#         print('You lose!')
 
 
-# def waiting_game():
-#     print('Press "s" to begin the timer.')
-
-#     while start_has_run == False:
-#         keyboard.add_hotkey('s', lambda: print(start_timer()), )
-
-#     while end_has_run == False:
-#         keyboard.add_hotkey('e', lambda: print(end_timer()))
+# waiting_game(3)
 
 
-# def start_timer():
-#     start_time = time.time()
-#     start_has_run = True
-#     return start_time
-
-
-# def end_timer():
-#     stop_time = time.time()
-#     end_has_run = True
-#     return stop_time
-
-
-# print(waiting_game())
